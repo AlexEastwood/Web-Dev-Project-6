6e8046ec8eeb1e95531d28b59aed3acfd1f836f4
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-09-06 20:25:20 +0100

    Added phrases array

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ const phrase = document. getElementById("phrase");
 const startBtn = document.querySelector(".btn__reset");
 let missed = 0; // tracks the number of wrong guesses the user has made
 
+const phrases = ["This is an example phrase", 
+"Bullfrogs are the largest frogs in North America", 
+"This is part of project six", 
+"More people have walked on the moon than won takeshis castle",
+"Pink isnt a real colour and doesnt exist"
+];
+
 startBtn.addEventListener("click", () => {
     const overlay = document.querySelector(".start");
     overlay.style.display = "none";
