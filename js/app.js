0597921e9d379a5b551602198deaf2feb972527b
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-09-06 20:21:20 +0100

    Start button works

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 const qwerty = document.getElementById("qwerty");
 const phrase = document. getElementById("phrase");
+const startBtn = document.querySelector(".btn__reset");
 let missed = 0; // tracks the number of wrong guesses the user has made
+
+startBtn.addEventListener("click", () => {
+    const overlay = document.querySelector(".start");
+    overlay.style.display = "none";
+});
